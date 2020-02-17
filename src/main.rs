5dca0b5d34834a0ad7d6c71fd8f848d91dc6a8af
Hendrik Sollich <hendrik@hoodie.de>
2020-02-17 15:29:38 +0100

    Add control and polymorphism

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+use std::cmp;
+use std::rc::Rc;
+use std::fmt;
+
+#[derive(Debug, Default)]
 struct Stereo {
-    active: bool,
+    volume: u16,
 }
 
 impl Stereo {
-    fn volume_up(&mut self) {}
-    fn volume_down(&mut self) {}
+    fn volume_up(&mut self) {
+        self.volume = cmp::min(self.volume + 1, 100);
+    }
+    fn volume_down(&mut self) {
+        self.volume = cmp::max(self.volume - 1, 0);
+    }
 }
 
-struct HotTub;
+#[derive(Debug, Default)]
+struct HotTub {
+    bubbles_on: bool,
+    heat_on: bool,
+}
 
 impl HotTub {
-    fn enable_bubbles(&mut self) {}
-    fn disable_bubbles(&mut self) {}
-    fn set_temperature(&mut self) {}
-    fn turn_off_heat(&mut self) {}
+    fn enable_bubbles(&mut self) {
+        self.bubbles_on = true
+    }
+    fn disable_bubbles(&mut self) {
+        self.bubbles_on = false
+    }
+    fn set_temperature(&mut self) {
+        self.heat_on = true
+    }
+    fn turn_off_heat(&mut self) {
+        self.heat_on = false
+    }
 }
 
-trait RemoteControllable {
+trait RemoteControllable: fmt::Debug {
     fn on_button_pressed(&mut self);
     fn off_button_pressed(&mut self);
 }
@@ -43,6 +64,25 @@ impl RemoteControllable for HotTub {
     }
 }
 
+#[derive(Debug, Default)]
+struct RemoteControl {
+    devices: Vec<Rc<dyn RemoteControllable>>,
+}
+
+impl RemoteControl {
+    fn add_device<T: RemoteControllable + 'static>(&mut self, device: Rc<T>) {
+        self.devices.push(device);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let hot_tub = Rc::new(HotTub::default());
+    let sound_blaster = Rc::new(Stereo::default());
+
+    let mut control = RemoteControl::default();
+    control.add_device(hot_tub.clone());
+    control.add_device(sound_blaster.clone());
+
+    dbg!(control);
 }
